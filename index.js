caf0263f5d0ac82775fc6a0952b906da778311a0
Kyle <ktvitale7@gmail.com>
2022-05-04 21:05:24 -0400

    commit tree 2ad8e0ac3e1cbdf9f5f2d3d52f1e9d7ae28e4c13

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCharacter = "Sid"
+
+function upperCaseCustomerName(){
+      customerName = customerName.toUpperCase()
+     return customerName
+     
+}
+
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    
+}
+bestCustomer()
+
+function overwriteBestCustomer(){
+     bestCustomer = "maybe bob"
+    
+}
+overwriteBestCustomer()
+
+
+// const leastFavoriteCharacter = "Sid"
+function changeLeastFavoriteCustomer(){
+        leastFavoriteCharacter = "Andy"
+
+}
